a074fb170271c4592e992e6100d86955d39e9ec8
kawser2133 <kawser2133@gmail.com>
2023-12-26 21:48:02 +0600

    U[pdated AppSettings Model

---
+++ Project.Core/Common/JwtConfig.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Project.Core.Common
+{
+    public class JwtConfig
+    {
+        public string? Secret { get; set; }
+        public string? ValidAudience { get; set; }
+        public string? ValidIssuer { get; set; }
+        public int TokenExpirationMinutes { get; set; }
+    }
+}
